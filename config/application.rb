01b8a845482a3b00f7e4593ec5d71d2cd4ec67c8
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-08-04 23:12:28 +1000

    Integrate s3 with paperclip

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ module KrissiUikitRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.paperclip_defaults = {
+      storage: :s3,
+      s3_protocol: 'https',
+      s3_credentials: {
+        bucket: Figaro.env.aws_bucket,
+        access_key_id: Figaro.env.aws_access_key_id,
+        secret_access_key: Figaro.env.aws_secret_access_key,
+        s3_region: Figaro.env.aws_region,
+        s3_host_name: Figaro.env.aws_endpoint
+      }
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
