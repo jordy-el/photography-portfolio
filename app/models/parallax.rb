0ff6d5483e91ce22d4cb1b77c0b6352a81e6c2bc
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-08-07 22:39:34 +1000

    Move default images to secure https

--- app/models/parallax.rb
+++ app/models/parallax.rb
@@ -1,6 +1,6 @@
 class Parallax < ApplicationRecord
   belongs_to :krissi
-  has_attached_file :image, default_url: 'http://via.placeholder.com/3840x2160.jpg'
+  has_attached_file :image, default_url: 'https://via.placeholder.com/3840x2160.jpg'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   before_validation :to_nil
   validate :only_three, on: :create
