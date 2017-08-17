0b35f1a60a153a299f7e3aa19b34a134bd3cc324
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-08-17 13:38:41 +1000

    Fix cookies - safari browser compatibility

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,8 +1,7 @@
 Clearance.configure do |config|
   config.routes = false
   config.allow_sign_up = false
-  config.mailer_sender = 'admin@krissiflynn.com.au'
   config.redirect_url = '/admin'
   config.rotate_csrf_on_sign_in = true
-  config.secure_cookie = true
+  config.secure_cookie = false
 end
